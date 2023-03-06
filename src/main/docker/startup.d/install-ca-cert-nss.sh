09c07868a4745beeddd8e37fee92007e8e0c66a3
kusumaghoshdastidar <kusumaghoshdastidar@gmail.com>
2023-03-06 14:59:21 -0800

    Add NSS cert util

---
+++ src/main/docker/startup.d/install-ca-cert-nss.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Copyright 2022-2023 Open Text.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+MESOS_SANDBOX=${SSL_CA_CRT_DIR:-$MESOS_SANDBOX}
+
+echo "Make /etc/pki/nssdb..."
+mkdir /etc/pki/nssdb
+
+echo "init nssdb..."
+certutil -N -d /etc/pki/nssdb/
+
+echo "Adding CA cert into NSS database on openSUSE"
+certutil -A -n test-cert -t “u,u,u” -d /etc/pki/nssdb/ -i $MESOS_SANDBOX/$SSL_CA_CRT
